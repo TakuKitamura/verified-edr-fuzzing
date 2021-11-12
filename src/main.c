00018d89a377f33aad44e0dbcb37b1046bc2ac28
TakuKitamura <takukitamura.io@gmail.com>
2021-11-12 15:59:03 +0900

    refacta

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main()
 {
     uint8_t buf[13] = {0};
     if (read(0, buf, 13) != 13)
-        return 0; // 渡されたFuzzが13バイト以上でない場合は終了し,先頭13バイトをbufに格納
+        return 0; // 渡されたFuzzが13バイト以上でない場合は終了し,先頭13バイトのみをbufに格納
 
     uint32_t can_id = buf[0] << 8 * 3 | buf[1] << 8 * 2 | buf[2] << 8 * 1 | buf[3]; // bufの先頭4byteをスライス
     uint8_t can_dlc = buf[4];
@@ -16,6 +16,7 @@ int main()
     for (int i = 5; i < 13; i++)
         data[i] = buf[i];
 
+    // fuzzing対象のF*で開発した3つの関数
     parseDoor(can_id, can_dlc, data);
     parseIndicator(can_id, can_dlc, data);
     parseSpeed(can_id, can_dlc, data);
